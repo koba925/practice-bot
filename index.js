680ae497dc52ca43328fcb9dad651b9d1dae3a1e
Takahiro Kobayashi <takahiro@jp.fujitsu.com>
2021-12-11 14:20:04 +0900

    Add Proxy Support

---
+++ index.js
@@ -0,0 +1,69 @@
+const { App } = require('@slack/bolt');
+const HttpsProxyAgent = require('https-proxy-agent');
+
+const proxy = process.env.http_proxy;
+const agent = new HttpsProxyAgent(proxy);
+
+const app = new App({
+  logLevel: 'debug',
+  socketMode: true,
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  agent
+});
+
+app.shortcut('global-shortcut', async ({ ack, body, client }) => {
+  await ack();
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      type: 'modal',
+      callback_id: 'modal-id',
+      title: {
+        type: 'plain_text',
+        text: 'タスク登録'
+      },
+      submit: {
+        type: 'plain_text',
+        text: '送信'
+      },
+      close: {
+        type: 'plain_text',
+        text: 'キャンセル'
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'input-task',
+          element: {
+            type: 'plain_text_input',
+            action_id: 'input',
+            multiline: true,
+            placeholder: {
+              type: 'plain_text',
+              text: 'タスクの詳細・期限などを書いてください'
+            }
+          },
+          label: {
+            type: 'plain_text',
+            text: 'タスク'
+          }
+        }
+      ]
+    }
+  });
+});
+
+app.view('modal-id', async ({ ack, view, logger }) => {
+  logger.info(`Submitted data: ${JSON.stringify(view.state.values)}`);
+  await ack();
+});
+
+app.message('こんにちは', async ({ message, say }) => {
+  await say(`:wave: こんにちは <@${message.user}>`);
+});
+
+(async () => {
+  await app.start();
+  console.log('PracticeBot started');
+})();
